42c209c9849584a690ba6082d26b3f0c7ffe336a
mehdi <0r0.developer@gmail.com>
2023-04-29 23:12:45 +0430

    implement aggregate root factory

--- DDDFramework.Tests/DDDFramework.Tests/EventStore/IEventStore.cs
+++ DDDFramework.Tests/DDDFramework.Tests/EventStore/IEventStore.cs
@@ -2,6 +2,9 @@
 
 namespace DDDFramework.Tests.EventStore;
 
+/// <summary>
+/// get events from event store by sending event stream Id to event store;
+/// </summary>
 public interface IEventStore
 {
     IReadOnlyCollection<DomainEvent> GetEvents(string eventStreamId);
