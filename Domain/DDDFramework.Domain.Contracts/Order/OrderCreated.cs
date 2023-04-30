1307766e8289b4ef39025035611e182e7f82bac3
mehdi <0r0.developer@gmail.com>
2023-05-01 01:04:19 +0430

    refactor code and define Order argument factory  base class

--- Domain/DDDFramework.Domain.Contracts/Order/OrderCreated.cs
+++ Domain/DDDFramework.Domain.Contracts/Order/OrderCreated.cs
@@ -6,13 +6,12 @@ public class OrderCreated : DomainEvent
 {
     public OrderCreated()
     {
-        
     }
 
-  
+    public OrderId Id { get; set; }
     public long OrderNumber { get; init; }
 
     public string Title { get; init; }
-    
+
     public bool IsActive { get; set; }
 }
