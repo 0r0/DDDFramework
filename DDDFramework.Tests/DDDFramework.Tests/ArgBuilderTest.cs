ef932b6fc29ef658949c3f0e861bc06d3b385f90
mehdi <0r0.developer@gmail.com>
2023-05-03 01:42:50 +0430

    make singleObject creator unit test and initiate implementaion

---
+++ DDDFramework.Tests/DDDFramework.Tests/ArgBuilderTest.cs
@@ -0,0 +1,15 @@
+﻿using DDDFramework.Domain.Contracts.Order;
+using DDDFramework.Domain.Order;
+
+namespace DDDFramework.Tests;
+
+public class ArgBuilderTest
+{
+    [Fact]
+    private void set_value_of_argument_that_created_by_single_object_builder()
+    {
+        var singleObject = new SingleObjectBuilder<OrderArgs,Guid>();
+        // singleObject.With(a=>a.Id.DbId=Guid.NewGuid(),Guid.NewGuid());
+
+    }
+}
--- Domain/DDDFramework.Domain/Order/OrderArgs.cs
+++ Domain/DDDFramework.Domain/Order/OrderArgs.cs
@@ -1,11 +1,47 @@
-﻿using DDDFramework.Domain.Contracts.Order;
+﻿using System.Reflection;
+using DDDFramework.Domain.Contracts.Order;
 
 namespace DDDFramework.Domain.Order;
 
-public interface ISingleObjectBuilder<out T> where T : class, new()
+public interface ISingleObjectBuilder< T,TProperty> where T : class, new()
 {
-    void With<TProperty>(Func<T, TProperty> func, TProperty value);
+    void With(Func<T, TProperty> func, TProperty value);
     T Build();
+}
+
+
+
+public class SingleObjectBuilder<T,TProperty> : ISingleObjectBuilder<T,TProperty> where T : class, new()
+{
+    public void With(Func<T, TProperty> func, TProperty value)
+    {
+        // Func<OrderArgs,Guid> m = M;
+        // var ty2 = typeof(OrderArgs).GetProperty("Id").SetValue(ty2,Guid.NewGuid());
+        // ty2.SetValue();
+        // Type ty = typeof(T);
+        // FieldInfo[] fieldInfos = ty.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+        // fieldInfos.Select()
+        var instance = Activator.CreateInstance<T>();
+        var a=  func(instance);
+        
+
+
+    }
+
+    private Guid M(OrderArgs arg)
+    {
+        Guid a = default;
+        arg.GetType().
+            GetField("Id",BindingFlags.Instance| BindingFlags.Public | BindingFlags.Static |BindingFlags.CreateInstance)
+            ?.SetValue(arg,a=Guid.NewGuid());
+        return a;
+
+    }
+
+    public T Build()
+    {
+        throw new NotImplementedException();
+    }
 }
 
 public class OrderArgs
@@ -14,7 +50,7 @@ public class OrderArgs
     {
     }
 
-    private OrderId Id { get; set; }
+    public OrderId Id { get; set; }
     private string Title { get; set; }
     private long OrderNumber { get; set; }
     private bool IsActive { get; set; }
