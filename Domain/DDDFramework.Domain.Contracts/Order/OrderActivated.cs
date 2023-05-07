c436d81653949fefbe33879fac6e35e2e585c88a
m.gohari <0r0.developer@gmail.com>
2023-05-07 15:50:25 +0330

    refactor OrderActivated event and  When method

--- Domain/DDDFramework.Domain.Contracts/Order/OrderActivated.cs
+++ Domain/DDDFramework.Domain.Contracts/Order/OrderActivated.cs
@@ -1,13 +1,10 @@
-﻿namespace DDDFramework.Domain.Contracts.Order;
+﻿using Aggregate;
 
-public class OrderActivated : OrderPlaced
+namespace DDDFramework.Domain.Contracts.Order;
+
+public class OrderActivated : DomainEvent
 {
     
 
-    public void ActivateOrder() => IsActive = true;
-
-    public OrderActivated(long orderNumber, string title) : base(orderNumber, title)
-    {
-        ActivateOrder();
-    }
+   
 }
