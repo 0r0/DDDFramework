8e640d5ab1ef488688059e9f430a987b11ebb6e6
m.gohari <0r0.developer@gmail.com>
2023-05-14 16:18:58 +0330

    configure order dependencies

---
+++ DDDFramework.Infrastructure/DDDFramework.Infrastructure.Config/OrderModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using DDDFramework.Application.Contracts;
+using DDDFramework.Application.Handlers;
+using DDDFramework.Domain.EventStore;
+using DDDFramework.Domain.Order;
+using Persistence.ES;
+
+namespace DDDFramework.Infrastructure.Config;
+
+public class OrderModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        base.Load(builder);
+        builder.RegisterGeneric(typeof(EventSourceRepository<,>)).As(typeof(IEventSourceRepository<,>))
+            .SingleInstance();
+        builder.RegisterType<OrderRepository>().As<IOrderRepository>().SingleInstance();
+        builder.RegisterAssemblyTypes(typeof(OrderCommandHandlers).Assembly)
+            .As(type => type.GetInterfaces().Where(t => t.IsClosedTypeOf(typeof(ICommandHandler<>))))
+            .InstancePerLifetimeScope();
+    }
+}
