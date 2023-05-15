8a0510ce27f1ebc5ade855795ef5e08eb461c028
m.gohari <0r0.developer@gmail.com>
2023-05-15 17:30:19 +0330

    dispatch post command in command post gateway

--- Gatways/Gateways/Controllers/OrderController.cs
+++ Gatways/Gateways/Controllers/OrderController.cs
@@ -1,18 +1,36 @@
-using DDDFramework.Application.Order;
+using DDDFramework.Application.Contracts.Orders;
 using DDDFramework.Core.Application.Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gateways.Controllers;
 
 [ApiController]
-[Route("api")]
+[Route("api/[controller]")]
 public class OrderController : ControllerBase
 {
     private readonly ICommandBus _commandBus;
 
-    [HttpPost("")]
+    public OrderController(ICommandBus commandBus)
+    {
+        _commandBus = commandBus;
+    }
+
+    [HttpPost]
     public IActionResult Post(CreateOrderCommand command)
     {
+        _commandBus.Dispatch(command);
         return Created("", command.Id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Put(Guid id, PlaceOrderCommand command)
+    {
+        return NoContent();
+    }
+
+    [HttpPut("activate/{id:guid}")]
+    public async Task<IActionResult> Put(Guid id, ActivateOrderCommand command)
+    {
+        return NoContent();
     }
 }
