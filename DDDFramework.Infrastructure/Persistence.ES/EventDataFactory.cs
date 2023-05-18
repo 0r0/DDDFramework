b241a84cc0e9ec3ec8a3960d60e3268c2cce86cb
mehdi <0r0.developer@gmail.com>
2023-05-18 13:53:47 +0430

    implement event data  factory for event store event data

---
+++ DDDFramework.Infrastructure/Persistence.ES/EventDataFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using Aggregate;
+using EventStore.ClientAPI;
+using Newtonsoft.Json;
+
+namespace Persistence.ES;
+
+public static class EventDataFactory
+{
+    public static EventData CreateFromDomainEvent(DomainEvent domainEvent)
+    {
+        var data = JsonConvert.SerializeObject(domainEvent);
+        return new EventData(eventId: domainEvent.EventId,
+            type: domainEvent.GetType().Name,
+            isJson: true,
+            data: Encoding.UTF8.GetBytes(data),
+            metadata: Array.Empty<byte>()
+        );
+    }
+
+    public static IReadOnlyCollection<EventData> CreateFromDomainEvents(IReadOnlyCollection<DomainEvent> domainEvents)
+    {
+        return domainEvents.Select(CreateFromDomainEvent).ToList();
+    }
+}
