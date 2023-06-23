4f0cb831ef141ee2c9d6fb79954ddfcd010fd6f0
mehdi <0r0.developer@gmail.com>
2023-06-24 02:27:58 +0430

    remove "Query" string from controller convention in gateway

---
+++ ServiceHost/Conventions/QueryControllerModelConvention.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace ServiceHost.Conventions;
+
+public class QueryControllerModelConvention : IApplicationModelConvention
+{
+    private const string ModelNameString = "Query";
+
+    public void Apply(ApplicationModel application)
+    {
+        foreach (var controller in application.Controllers.Where(a => a.ControllerName.EndsWith(ModelNameString)))
+        {
+            foreach (var selectorModel in controller.Selectors.Where(selector => selector.AttributeRouteModel != null))
+            {
+                selectorModel.AttributeRouteModel = new AttributeRouteModel()
+                {
+                    Template = "api/" + controller.ControllerName.Replace(ModelNameString, "")
+                };
+            }
+        }
+    }
+}
