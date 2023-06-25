66612b7043754730b02bb119dc1745a830ea1857
m.gohari <0r0.developer@gmail.com>
2023-06-25 15:14:41 +0330

    add healthcheck ui and related configuration to project

--- ServiceHost/InfrastructureHealthCheck.cs
+++ ServiceHost/InfrastructureHealthCheck.cs
@@ -1,13 +1,33 @@
+using EventStore.Client;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace ServiceHost;
 
-public class InfrastructureHealthCheck :IHealthCheck
+public class InfrastructureHealthCheckEventStore : IHealthCheck,IDisposable
 {
-    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
-    {
-        //todo! implement event store connection health check here
-        throw new NotImplementedException();
+    private readonly string? _connectionString;
+    private readonly EventStoreClient _client;
 
+    public InfrastructureHealthCheckEventStore(string? connectionString)
+    {
+        ArgumentNullException.ThrowIfNull(_connectionString);
+        _client = new EventStoreClient(EventStoreClientSettings
+            .Create(_connectionString));
+        _connectionString = connectionString;
     }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        if (string.IsNullOrEmpty(_connectionString))
+            return new HealthCheckResult(context.Registration.FailureStatus,
+                "event store connection address is not defined");
+
+        return await _client.ReadAllAsync(Direction.Forwards, Position.Start, 1, cancellationToken: cancellationToken)
+            .Messages.AnyAsync(cancellationToken)
+            ? HealthCheckResult.Healthy("event store connection is existed")
+            : new HealthCheckResult(context.Registration.FailureStatus, "Failed to connect to EventStore.");
+    }
+    public void Dispose() => _client.Dispose();
+
 }
--- ServiceHost/Program.cs
+++ ServiceHost/Program.cs
@@ -1,6 +1,9 @@
+using System.Diagnostics;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using DDDFramework.Infrastructure.Config;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using ServiceHost;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,17 +17,27 @@ builder.Services.AddControllersInGateways();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
-builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecksUI(setupSettings: setup =>
+{
+    setup.SetEvaluationTimeInSeconds(5); 
+    setup.AddHealthCheckEndpoint("infrastructure", "https://localhost:7257/healthz");
+
+}).AddInMemoryStorage();
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-var eventStorSettings = new Settings();
-builder.Configuration.GetSection("EventStoreConnection").Bind(eventStorSettings);
+var eventStoreSettings = new Settings();
+builder.Configuration.GetSection("EventStoreConnection").Bind(eventStoreSettings);
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 
 builder.Host.ConfigureContainer<ContainerBuilder>(autofacBuilder =>
-    autofacBuilder.RegisterModule(new OrderModule(eventStorSettings.Url)));
+    autofacBuilder.RegisterModule(new OrderModule(eventStoreSettings.Url)));
 
+Debug.Assert(eventStoreSettings.Url != null, "eventStoreSettings.Url != null");
+builder.Services.AddHealthChecks().AddEventStore(eventStoreSettings.Url);
+    // .AddCheck("event-store",new InfrastructureHealthCheckEventStore(eventStoreSettings.Url))
+    // .AddEventStore(eventStoreSettings.Url);
+// builder.Services.AddHealthChecksUi.AddInMemoryStorage();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,4 +54,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.MapHealthChecks("/healths");
+app
+    .UseRouting()
+    .UseEndpoints(config =>
+    {
+        config.MapHealthChecks("/healthz", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+        config.MapHealthChecksUI(setup =>
+        {
+            setup.AddCustomStylesheet("styles/dotnet.css");
+        });
+    });
+
 app.Run();
