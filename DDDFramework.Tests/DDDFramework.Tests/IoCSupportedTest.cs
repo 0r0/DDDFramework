49d1070e63c82e99671ef7a8fcf7152af1b36ebc
mehdi <0r0.developer@gmail.com>
2023-06-30 19:30:50 +0430

    implement iocSupporter for unit test

---
+++ DDDFramework.Tests/DDDFramework.Tests/IoCSupportedTest.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Autofac.Core;
+
+namespace DDDFramework.Tests;
+
+
+    public class IoCSupportedTest<TModule> where TModule : IModule, new()
+    {
+        private IContainer _container;
+
+        public IoCSupportedTest()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterModule(new TModule());
+
+            _container = builder.Build();
+        }
+
+        protected TEntity Resolve<TEntity>()
+        {
+            
+            return _container.Resolve<TEntity>();
+        }
+
+        protected void ShutdownIoC()
+        {
+            _container.Dispose();
+        }
+    }
