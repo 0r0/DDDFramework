e889eb1d5a98529196d75ba29466e9970719bc1f
m.gohari <0r0.developer@gmail.com>
2023-07-03 09:54:30 +0330

    fix typo

--- Aggregate/AggregateRoot.cs
+++ Aggregate/AggregateRoot.cs
@@ -2,43 +2,43 @@
 
 public abstract class AggregateRoot<TKey> : Entity<TKey>, IAggregateRoot
 {
-    public List<DomainEvent> _uncommitedEvent;
+    public readonly List<DomainEvent> _uncommittedEvent;
 
     public long Version { get; private set; }
 
     protected AggregateRoot()
     {
-        _uncommitedEvent = new List<DomainEvent>();
+        _uncommittedEvent = new List<DomainEvent>();
     }
 
-    public IReadOnlyList<DomainEvent> GetUncommittedEvents() => _uncommitedEvent.AsReadOnly();
+    public IReadOnlyList<DomainEvent> GetUncommittedEvents() => _uncommittedEvent.AsReadOnly();
 
 
     public void AddEvent(DomainEvent @event)
 
     {
         if (@event is null) throw new ArgumentNullException($"domain event can  not be null=>{nameof(@event)}");
-        _uncommitedEvent.Add(@event);
+        _uncommittedEvent.Add(@event);
     }
 
     public virtual void RemoveEvent(DomainEvent @event)
     {
         if (@event is null) throw new ArgumentNullException($"domain event can  not be null=>{nameof(@event)}");
 
-        _uncommitedEvent.Remove(@event);
+        _uncommittedEvent.Remove(@event);
     }
 
     public void UpdateVersion() => ++Version;
 
     public void SetAggregateVersion() => ++Version;
     public void SetDomainEventVersion(DomainEvent @event) => @event.Version = Version + 1L;
-    public void ClearUncommittedEvent() => _uncommitedEvent.Clear();
+    public void ClearUncommittedEvent() => _uncommittedEvent.Clear();
 
     public abstract void Apply(DomainEvent @event);
 
     public void ApplyAndPublish(DomainEvent @event)
     {
-        _uncommitedEvent.Add(@event);
+        _uncommittedEvent.Add(@event);
         this.Apply(@event);
     }
 }
