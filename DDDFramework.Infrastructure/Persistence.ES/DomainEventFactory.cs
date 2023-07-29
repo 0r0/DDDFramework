b82938e27e379e0a9c0f7e95a934a5685bfd5616
m.gohari <0r0.developer@gmail.com>
2023-07-29 16:28:02 +0330

    add apply mapping method

--- DDDFramework.Infrastructure/Persistence.ES/DomainEventFactory.cs
+++ DDDFramework.Infrastructure/Persistence.ES/DomainEventFactory.cs
@@ -1,6 +1,8 @@
 ﻿using Aggregate;
 using EventStore.Client;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Persistence.ES.Mapping;
 
 namespace Persistence.ES;
 
@@ -15,7 +17,19 @@ internal static class DomainEventFactory
     public static DomainEvent Create(ResolvedEvent resolvedEvent, IEventTypeResolver resolver)
     {
         var type = resolver.GetType(resolvedEvent.Event.EventType);
-        var body = resolvedEvent.Event.Data.ToString();
-        return JsonConvert.DeserializeObject<DomainEvent>(body)  ?? throw new InvalidOperationException();
+
+        var body = ApplyMapping(resolvedEvent.Event.Data.ToString(), type);
+        return JsonConvert.DeserializeObject<DomainEvent>(body) ?? throw new InvalidOperationException();
+    }
+
+    private static string ApplyMapping(string body, Type type)
+    {
+        var filter = SchemaMappingRegistration.GetFilterByType(type);
+        
+        if (filter is null) return body;
+        
+        var filteredBody = filter.Apply(JObject.Parse(body));
+        
+        return filteredBody.ToString();
     }
 }
